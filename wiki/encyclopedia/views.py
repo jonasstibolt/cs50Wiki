292eebf21d4147a77ddd8ccfca1e681c95b78340
jonasstibolt <jonasholmgaard@gmail.com>
2024-03-22 19:21:04 +0100

    add .gitignore

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,10 +4,11 @@ from markdown2 import Markdown
 
 from . import util
 
+#vars for converting md to html
 markdowner = Markdown()
-
 page = util.get_entry()
 page_converted = markdowner.convert(page)
+#end
 
 def index(request):
     return render(request, "encyclopedia/index.html", {

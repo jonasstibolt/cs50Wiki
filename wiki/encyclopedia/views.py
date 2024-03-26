21c65a628424919353bef32c0755796c90a9d6c9
jonasstibolt <jonasholmgaard@gmail.com>
2024-03-26 22:20:57 +0100

    task one COMPLETE

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
 from markdown2 import Markdown
-
 from . import util
 from .util import fuck_generator
 
 
 #vars for converting md to html
 markdowner = Markdown()
-page = util.get_entry()
-page_converted = markdowner.convert(page)
 # end
 
 def index(request):
@@ -18,10 +14,17 @@ def index(request):
     })
 
 # start new view (maybe title instead of page?)
-def entry(request, title): #added entry name as parameter
-    return render(request, f"encyclopedia/index/{title}.html", {
-        "page": page_converted
-    })
+def entry(request, title):
+    page = util.get_entry(title)  # Pass the title to get_entry
+    if page:
+        page_converted = markdowner.convert(page)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "page": page_converted
+        })
+    else:
+        return render(request, "encyclopedia/404.html")
+    
 # end new view
 
 def test(request):
